3e1eef9a9158041cf741a5a469ffabe220221523
luis54ber70-star <luis54ber.70@gmail.com>
2026-04-02 18:30:03 -0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Chat from './components/Chat';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-gray-100 font-sans">
+      <Chat />
+    </div>
+  );
+}
